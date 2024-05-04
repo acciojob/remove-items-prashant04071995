7aac9641a2279d849e6a40e2f71f05ba0074b5d7
Prashant Salve <128687203+prashant04071995@users.noreply.github.com>
2024-05-04 13:04:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function removeSelectedColor() {
+    // Get the dropdown list by its ID
+    var selectElement = document.getElementById('colorSelect');
+    
+    // Get the index of the currently selected option
+    var selectedIndex = selectElement.selectedIndex;
+
+    // Remove the selected option
+    selectElement.remove(selectedIndex);
+}
